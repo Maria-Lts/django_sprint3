5e499b893af8e108ca75c3ac05d1a9484dfd41ed
Мария Луцюк <mashaverstuckowa@yandex.ru>
2023-09-04 21:40:42 +0300

    Add new changes

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -10,7 +10,8 @@ class BaseModel(models.Model):
         default=True, verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
         )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Добавлено')
 
     class Meta:
         abstract = True
@@ -21,8 +22,9 @@ class Post(BaseModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
-        )
+        help_text=('Если установить дату и время в будущем — '
+                   'можно делать отложенные публикации.'
+                   ))
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                verbose_name='Автор публикации')
     location = models.ForeignKey('Location', on_delete=models.SET_NULL,
@@ -45,8 +47,10 @@ class Category(BaseModel):
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(
         unique=True, verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
-        )
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+            ))
 
     class Meta:
         verbose_name = 'категория'
